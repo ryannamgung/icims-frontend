d3ad1b92f3f0a38b6179bed7c5ee55e5d1943aa2
ryannamgung <ryannamgung@gmail.com>
2018-10-27 09:14:24 -0400

    new repo

---
+++ src/AfnanNoWakey.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class AfnanNoWakey extends Component{
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  render(){
+    return (
+      <h2>Afnan No Wakey....</h2>
+    )
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AfnanNoWakey from "./AfnanNoWakey.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Austins Website</h1>
+        <div> austin is like 21 ish and he does stuff at Rowan. Lifts frequently with stickbug Matthew Lawerence. </div>
+        <AfnanNoWakey />
       </div>
     );
   }
